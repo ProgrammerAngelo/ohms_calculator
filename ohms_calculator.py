1e3d3aeb1cef5d61537354f35b821155e239f53b
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-09-27 06:45:39 -0400

    added a choices asking the user to what to calculate

--- ohms_calculator.py
+++ ohms_calculator.py
@@ -1,2 +1,15 @@
 #add a method to ask the user what to calculate
-#add a method for invalid input
+def ohms_calculator():
+    #for asking the user for input if invalid it will ask again
+    while True:
+        print("What to calculate?\nInput 1. for Voltage (V)\nInput 2. for Current  (I)\nInput 3. Resistance (R)")
+        choice = int(input("Input 1,2 or 3: "))
+        #for getting user choice
+        if choice in [1,2,3]:
+            break
+        #for invalid input
+        else:
+            print("Invalid. Please input 1,2 or 3")
+
+ohms_calculator()
+
